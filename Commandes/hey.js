1332f115b48b9580d78890168e8125c6b4b9a0cc
Aurélien LEMAIRE <lemr.aurelien@gmail.com>
2024-05-13 23:47:11 +0200

    Ajout de la commande Hey!

---
+++ Commandes/hey.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js")
+
+module.exports = {
+    name: "hey",
+    description: "Sois poli, dis bonjour nan?",
+    permission: "Aucune",
+    dm: false,
+
+    async run(bot, message) {
+        await message.reply(`hey!`);
+    }
+}
